237aa7e20e9371ebc461b831faea5ac2987311e1
hamidafaizal <hamidafaizalamin0@gmail.com>
2026-08-27 23:21:09 +0700

    fix2

--- vite.config.js
+++ vite.config.js
@@ -5,28 +5,43 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     react(),
+
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
+
+      includeAssets: [
+        'logo-192.png',
+        'logo-512.png'
+      ],
+
       manifest: {
         name: 'SOM — Kartika Accessories Ponorogo',
+
         short_name: 'SOM Kartika',
-        description: 'Aplikasi Stock Opname Kartika Accessories',
+
+        description:
+          'Aplikasi Stock Opname Kartika Accessories',
+
         theme_color: '#E51B23',
+
         background_color: '#FFFFFF',
+
         display: 'standalone',
+
         orientation: 'portrait',
+
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'logo-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'logo-512.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
